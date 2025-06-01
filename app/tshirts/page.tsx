3da8091ce1c2c3ce46f4f429b17c2cbe19b88e58
Ashish Kumar <ns730106@gmail.com>
2025-06-01 07:10:21 +0530

    styled t-shirt page

--- app/tshirts/page.tsx
+++ app/tshirts/page.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function Tshirts() {
   return (
@@ -7,140 +8,110 @@ function Tshirts() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <Link
+              href="/product/buy-ora"
+              className="lg:w-1/4 md:w-1/2 p-4 w-full block shadow-lg"
+            >
               <div className="relative h-48 rounded overflow-hidden">
                 <Image
                   alt="t-shirt"
                   src="/t-shirt-image.jpg"
-                  layout="fill"
-                  objectFit="cover"
+                  height={120}
+                  width={150}
+                  className="m-auto md:m-0"
                 />
               </div>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                <p className="mt-1">$16.00</p>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Shooting
+                </h2>
+                <p className="mt-1">$21.15</p>{" "}
+                <p className="mt-1">S, M, L, XL, XXL</p>
               </div>
-            </div>
+            </Link>
 
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <Link
+              href="/product/buy-ora"
+              className="lg:w-1/4 md:w-1/2 p-4 w-full block shadow-lg"
+            >
               <div className="relative h-48 rounded overflow-hidden">
                 <Image
                   alt="t-shirt"
                   src="/t-shirt-image.jpg"
-                  layout="fill"
-                  objectFit="cover"
+                  height={120}
+                  width={150}
+                  className="m-auto md:m-0"
                 />
               </div>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                <p className="mt-1">$21.15</p>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Shooting
+                </h2>
+                <p className="mt-1">$21.15</p>{" "}
+                <p className="mt-1">S, M, L, XL, XXL</p>
               </div>
-            </div>
+            </Link>
 
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <Link
+              href="/product/buy-ora"
+              className="lg:w-1/4 md:w-1/2 p-4 w-full block shadow-lg"
+            >
               <div className="relative h-48 rounded overflow-hidden">
                 <Image
                   alt="t-shirt"
                   src="/t-shirt-image.jpg"
-                  layout="fill"
-                  objectFit="cover"
+                  height={120}
+                  width={150}
+                  className="m-auto md:m-0"
                 />
               </div>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                <p className="mt-1">$12.00</p>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Shooting
+                </h2>
+                <p className="mt-1">$21.15</p>{" "}
+                <p className="mt-1">S, M, L, XL, XXL</p>
               </div>
-            </div>
+            </Link>
 
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <Link
+              href="/product/buy-ora"
+              className="lg:w-1/4 md:w-1/2 p-4 w-full block shadow-lg"
+            >
               <div className="relative h-48 rounded overflow-hidden">
                 <Image
                   alt="t-shirt"
                   src="/t-shirt-image.jpg"
-                  layout="fill"
-                  objectFit="cover"
+                  height={120}
+                  width={150}
+                  className="m-auto md:m-0"
                 />
               </div>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                <p className="mt-1">$18.40</p>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Shooting
+                </h2>
+                <p className="mt-1">$21.15</p>{" "}
+                <p className="mt-1">S, M, L, XL, XXL</p>
               </div>
-            </div>
-
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <div className="relative h-48 rounded overflow-hidden">
-                <Image
-                  alt="t-shirt"
-                  src="/t-shirt-image.jpg"
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                <p className="mt-1">$16.00</p>
-              </div>
-            </div>
-
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <div className="relative h-48 rounded overflow-hidden">
-                <Image
-                  alt="t-shirt"
-                  src="/t-shirt-image.jpg"
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                <p className="mt-1">$21.15</p>
-              </div>
-            </div>
-
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <div className="relative h-48 rounded overflow-hidden">
-                <Image
-                  alt="t-shirt"
-                  src="/t-shirt-image.jpg"
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                <p className="mt-1">$12.00</p>
-              </div>
-            </div>
-
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <div className="relative h-48 rounded overflow-hidden">
-                <Image
-                  alt="t-shirt"
-                  src="/t-shirt-image.jpg"
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                <p className="mt-1">$18.40</p>
-              </div>
-            </div>
-
+            </Link>
           </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
 
-export default Tshirts
+export default Tshirts;
