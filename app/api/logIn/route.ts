69c8dc45157311d664154fa2ccb20fa93b5e0cbb
Ashish Kumar <ns730106@gmail.com>
2025-06-30 10:23:27 +0530

    added cryptojs for password

--- app/api/logIn/route.ts
+++ app/api/logIn/route.ts
@@ -1,33 +1,47 @@
 import { NextResponse } from "next/server";
 import User from "@/models/User";
 import connectDb from "@/middleware/mongoose";
+import CryptoJS from "crypto-js";
+import jwt from "jsonwebtoken";
 
 export async function POST(req: Request) {
   await connectDb();
   const body = await req.json();
   try {
-    const {password}= body
-    const user = await User.findOne({email:body.email})
-    if(user){
-      if(password === user.password){
+    const { password } = body;
+    const user = await User.findOne({ email: body.email });
+    if (user) {
+      const bytes = CryptoJS.AES.decrypt(user.password, "secrwt123");
+      const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
+      if (password === decryptedPassword) {
+        const token = jwt.sign(
+          {
+            Name: user.Name,
+            email: user.email,
+          },
+          "your_jwt_secret", 
+          { algorithm: "HS256", expiresIn: "7d" }
+        );
         return NextResponse.json(
-      { sucess: true, message: "you logged in", Name:user.Name ,email:user.email},
-      { status: 200 }
-        )
-      }else {
+          {
+            token,
+          },
+          { status: 200 }
+        );
+      } else {
         return NextResponse.json(
           { success: false, error: "Invalid credentials" },
           { status: 401 }
         );
       }
-    }else {
+    } else {
       return NextResponse.json(
         { success: false, error: "User not found" },
         { status: 404 }
       );
-    }      
+    }
   } catch (error) {
- console.error(error);
+    console.error(error);
     return NextResponse.json(
       { sucess: false, error: "Internal server error while logging in" },
       { status: 400 }
--- app/api/signUp/route.ts
+++ app/api/signUp/route.ts
@@ -1,25 +1,30 @@
 import { NextResponse } from "next/server";
 import User from "@/models/User";
 import connectDb from "@/middleware/mongoose";
+import CryptoJS from "crypto-js";
 
 export async function POST(req: Request) {
   await connectDb();
   const body = await req.json();
   try {
-    const {Name, email, password}= body
-    const u=new User({
+    const { Name, email } = body;
+    const u = new User({
       Name,
       email,
-      password
-    })
-    await u.save()
-     return NextResponse.json(
-      { sucess: true, message: "your account is created" },
-      { status: 200 })
-  } catch (error) {
- console.error(error);
+      password: CryptoJS.AES.encrypt(body.password, "secrwt123").toString(),
+    });
+    await u.save();
     return NextResponse.json(
-      { sucess: false, error: "internal server error in creating your account" },
+      { sucess: true, message: "your account is created" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        sucess: false,
+        error: "internal server error in creating your account",
+      },
       { status: 400 }
     );
   }
